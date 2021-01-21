d02c905ce453c1a431e42ff1836a4063053f6db0
William Ochomo <ochomoswill@Devs-MacBook-Pro.local>
2021-01-21 16:35:12 +0300

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import List from 'devextreme-react/list';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+
+interface IListItemProps {
+  text: string;
+}
+
+interface IComponentState {
+  counter: number;
+}
+
+const items: IListItemProps[] = [
+  { text: "Item 1" },
+  { text: "Item 2" },
+  { text: "Item 3" }
+];
+
+class Item extends React.Component<IListItemProps, IComponentState> {
+  constructor(props: IListItemProps) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.addCounter = this.addCounter.bind(this);
+  }
+
+  public render() {
+    return (
+        <div onClick={this.addCounter}>
+          {this.props.text} was clicked {this.state.counter} times
+        </div>
+    );
+  }
+
+  private addCounter() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+        <List items={items} itemComponent={Item} />
+    );
+  }
 }
 
 export default App;
